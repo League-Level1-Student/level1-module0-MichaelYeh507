efc199f375781a36292d5faa92a6ae3b9d5565bb
MichaelYeh507 <54426940+MichaelYeh507@users.noreply.github.com>
2020-01-27 19:49:56 -0800

    Sound effects machine(ip)

--- src/_09_sound_effects_machine/SoundEffectsMachine.java
+++ src/_09_sound_effects_machine/SoundEffectsMachine.java
@@ -1,12 +1,26 @@
 package _09_sound_effects_machine;
 
+import java.applet.AudioClip;
+
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
 
+@SuppressWarnings("deprecation")
 public class SoundEffectsMachine implements ActionListener {
 	public void showButton() {
 		System.out.println("Button clicked");
@@ -21,12 +35,31 @@ public class SoundEffectsMachine implements ActionListener {
 		frame.pack();
 		leftButton.addActionListener(this);
 		rightButton.addActionListener(this);
-
 	}
-
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		playSound("sawing-wood-daniel_simon.wav");
+	}
+
+	private void playSound(String fileName) {
 		
+//		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+//		sound.play();
+
+		try {
+			File musicPath = new File(fileName);
+			
+			if (musicPath.exists()) {
+				AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+				Clip clip = AudioSystem.getClip();
+				JOptionPane.showMessageDialog(null, "Press OK to stop playing");
+				
+			} else {
+				System.out.println("Can't find file");
+			}
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 }
